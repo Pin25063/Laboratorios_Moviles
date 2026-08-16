71b1dae9f7ddfffebb16317def9e4763976ef164
JP <pin25063@uvg.edu.gt>
2026-08-16 08:59:52 -0600

    Lab4

---
+++ Lab4/app/src/main/java/plat/lab4/pinto_ramirez/MainActivity.kt
@@ -0,0 +1,140 @@
+package plat.lab4.pinto_ramirez
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
+import plat.lab4.pinto_ramirez.ui.theme.Lab4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    UiPrincipal(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(0xFF2E7D32))
+                            .padding(innerPadding)
+                            .background(Color.White)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UiPrincipal(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.uvg_logo),
+            contentDescription = null,
+            modifier = Modifier.height(300.dp).width(300.dp),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Fit,
+            alpha = 0.15f // 15% de opacidad
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Text(
+                    text = "Universidad del Valle de Guatemala",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "\nProgramación de Plataformas moviles, Sección 30",
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Column { Text(text = "Integrantes", fontWeight = FontWeight.Bold) }
+                Spacer(modifier = Modifier.width(50.dp))
+                Column { Text("Jose Pinto\nCristian Ronaldo\nLionel Pessi") }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Column { Text(text = "Catedratico", fontWeight = FontWeight.Bold) }
+                Spacer(modifier = Modifier.width(50.dp))
+                Column { Text("Juan Carlos Durini") }
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Jose Pinto ")
+                Text("\n25063")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab4Theme {
+        UiPrincipal(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF2E7D32))
+                .padding(30.dp)
+                .background(Color.White)
+        )
+    }
+}
